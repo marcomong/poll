2e141382232e256cd5dcf0cc870f76cb1e7f2185
Marco Mongelli <mongelli.marco.8@gmail.com>
2019-11-25 18:45:04 -0600

    adds keys for production in web app

--- web/src/configuration/config.js
+++ web/src/configuration/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   app: {
-    url: process.env.WEB_URL || 'http://localhost',
+    url: process.env.VUE_APP_WEB_URL || 'http://localhost',
     env: process.env.NODE_ENV || 'dev'
   },
   axios: {
-    baseInstance: process.env.AXIOS_INSTANCE || 'http://localhost:8081/',
-    authInstance: process.env.AXIOS_INSTANCE_AUTH || 'http://localhost:8082/',
-    pollInstance: process.env.AXIOS_INSTANCE_POLL || 'http://localhost:8083/'
+    baseInstance: process.env.VUE_APP_AXIOS_INSTANCE || 'http://localhost:8081/',
+    authInstance: process.env.VUE_APP_AXIOS_INSTANCE_AUTH || 'http://localhost:8082/',
+    pollInstance: process.env.VUE_APP_AXIOS_INSTANCE_POLL || 'http://localhost:8083/'
   }
 }
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -32,6 +32,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+
 export default {
   computed: {
     ...mapGetters(['isUserLoggedIn']),
